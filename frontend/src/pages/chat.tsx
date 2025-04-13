c5bb7c174bd1dc7334ae508ca50d026f983c704e
Eric Wong <107148516+ewbyf@users.noreply.github.com>
2025-04-12 23:26:17 -0500

    updated chat ui

--- frontend/src/pages/chat.tsx
+++ frontend/src/pages/chat.tsx
@@ -7,8 +7,9 @@ import { InferGetServerSidePropsType } from 'next';
 import Logo from '@/components/Logo';
 import Scene from '@/components/CatScene';
 import ChatMessage from '@/components/ChatMessage';
-import { Send } from 'lucide-react';
 import UserCamera from '@/components/UserCamera';
+import { LogOut, Mic, Send, MicOff, PhoneCall } from 'lucide-react';
+import { useState } from 'react';
 
 export const getServerSideProps = async () => {
 	const accessToken = await fetchAccessToken({
@@ -35,22 +36,39 @@ export const getServerSideProps = async () => {
 type PageProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 
 export default function Page({ accessToken }: PageProps) {
+	const [muted, setMuted] = useState(false);
+    const [sessionStarted, setSessionStarted] = useState(false);
+
 	return (
 		// <VoiceProvider auth={{ type: 'accessToken', value: accessToken }}>
 		// 	<Messages />
 		// 	<Controls />
 		// </VoiceProvider>
-		<div className="bg-gradient-to-br from-white to-[#B4F7F8] h-screen flex flex-row">
+		<div className="bg-gradient-to-r from-white via-white via-30% to-[#B4F7F8] h-screen flex flex-row">
 			<Logo />
-			<div className="w-3/4 flex flex-col p-16 gap-8">
+			<div className="w-3/4 flex flex-col p-8 pt-24 gap-8">
 				<Scene>
-					<div className="absolute -top-8 -right-8 h-40 w-auto rounded-2xl">
+					<div className="absolute -top-8 -right-4 h-40 w-auto rounded-2xl">
 						<UserCamera />
 					</div>
 				</Scene>
-				<div className="flex flex-row justify-center items-center gap-16">
-					<button className="px-16 py-5 rounded-md bg-red-500 text-white">End Session</button>
-					<button className="p-8 rounded-full bg-neutral-300">End</button>
+				<div className="flex flex-row justify-center items-center gap-6">
+					{sessionStarted ? <button className="px-16 py-5 rounded-md bg-red-500 text-white flex gap-3 hover:bg-red-600">
+						<LogOut />
+						End Session
+					</button> : <button className="px-16 py-5 rounded-md bg-primary text-white flex gap-3 hover:bg-primaryhover">
+						<PhoneCall />
+						Start Session
+					</button>}
+					<button className="p-5 rounded-full bg-neutral-300 hover:bg-neutral-400" onClick={() => setMuted(!muted)}>
+						{muted ? <MicOff /> : <Mic />}
+					</button>
+                    {/* <button
+						className="p-5 rounded-full bg-white/30 hover:bg-white/40 backdrop-blur-md border border-white/20 shadow-sm transition duration-200"
+						onClick={() => setMuted(!muted)}
+					>
+						{muted ? <MicOff /> : <Mic />}
+					</button> */}
 				</div>
 			</div>
 			<div className="bg-white w-1/4 flex flex-col gap-6">
