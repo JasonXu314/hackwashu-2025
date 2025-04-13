e6c8a55d1fa498fbbe7c8d7a23637e7933e9fbd5
Matthew <68716884+2406mmartin@users.noreply.github.com>
2025-04-12 19:59:38 -0500

    Fix

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -8,14 +8,12 @@ export default function Page() {
                     <Scene>
                         <div className="bg-neutral-300 top-16 right-16 w-20 h-20 rounded-3xl p-16 absolute z-[99]">YOU</div>
                     </Scene>
-                    <div className="flex flex-row justify-center gap-16 h-full">
+                    <div className="flex flex-row justify-center gap-16">
                         <button className="p-8 bg-red-500 rounded-md">End Session</button>
                         <button className="p-8 rounded-full bg-neutral-300">End</button>
                     </div>
                 </div>
-                <div className='bg-neutral-300 basis-1/3'>
-                    
-                </div>
+                <div className='bg-neutral-300 basis-1/3'>CHATTY</div>
             </div>
         </>
     );
